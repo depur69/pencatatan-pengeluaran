984f4daa9ca84a717e217c206d07b0332c5bf2c8
depur69 <depurrr69@gmail.com>
2026-01-15 19:53:39 +0800

    menambahkan komentar

--- category/delete.php
+++ category/delete.php
@@ -1,4 +1,19 @@
 <?php
+// Memanggil file database untuk koneksi dan session
 include "../config/database.php";
-mysqli_query($conn,"DELETE FROM categories WHERE id='{$_GET['id']}'");
+
+// Mengambil id kategori dari parameter URL
+$id = $_GET['id'];
+
+// Menjalankan query untuk menghapus data kategori
+// berdasarkan id yang dipilih
+mysqli_query(
+    $conn,
+    "DELETE FROM categories WHERE id='$id'"
+);
+
+// Mengarahkan kembali ke halaman index kategori
 header("Location: index.php");
+
+// Menghentikan eksekusi script
+exit;
