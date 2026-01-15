a248d778122f806b902fce23590b9c74fd5c5601
Pramartha90 <madepramartha7@gmail.com>
2026-01-15 20:42:08 +0800

    Pembaruan logout.php

--- auth/logout.php
+++ auth/logout.php
@@ -1,4 +1,9 @@
 <?php
+// Memulai session agar bisa diakses/dihapus
 session_start();
+
+// Menghancurkan semua data session (logout user)
 session_destroy();
+
+// Redirect ke halaman login setelah logout
 header("Location: login.php");
